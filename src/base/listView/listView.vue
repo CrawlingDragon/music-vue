51123a16d52194243d7d0a784e019c79fe42677e
CrawlingDragon <363630867@qq.com>
2017-12-07 20:06:31 +0800

    singer
    singer组件

---
+++ src/base/listView/listView.vue
@@ -0,0 +1,30 @@
+<template>
+    <scroll>
+
+    </scroll>
+</template>
+
+<script type="text/ecmascript-6">
+  import Scroll from 'base/scroll/scroll'
+	export default {
+    prop:{
+      data:{
+        type:Array,
+        default:[]
+      }
+    },
+		data() {
+			return {}
+		},
+		created() {
+		},
+		mounted() {
+		},
+		methods: {}
+	}
+
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,13 +1,17 @@
 <template>
-  <div>
-
+  <div class="singer" ref="singer">
+    <!--<list-view :data="singers"></list-view>-->
   </div>
 </template>
 
 <script type="text/ecmascript-6">
   import {getSingers} from 'api/singer'
   import {ERR_OK} from 'api/config'
+  // import ListView from 'base/listView/listView'
+  import Singer from 'common/js/singer'
 
+  const HOT_NAME = '热门'    //定义hotname title
+  const HOT_SINGER_LEN = 10 // 定义数据的前10条为热门数据
   export default {
     data() {
       return {
@@ -16,23 +20,81 @@
     },
     created() {
       this._getSingers()
+
     },
     mounted() {
     },
     methods: {
-      _getSingers() {
+      _getSingers() {  //获取歌手数据
         getSingers().then((res) => {
           if (res.code === ERR_OK) {
-            this.singers = res.data.list
-            console.log(res.data.list)
+            this.singers = this._normalizeSinger(res.data.list)
+            console.log(this.singers)
           }
         })
+      },
+      _normalizeSinger(list) {
+        let map = {  //定义热门栏目对象
+          hot: {
+            title: HOT_NAME,
+            items: []
+          }
+        }
+
+        list.forEach((item, index) => { //定义a-b的列表对象
+          if (list.indexOf(item) < HOT_SINGER_LEN) { // 取list数据的前10条给hot栏目
+            map.hot.items.push(new Singer({  //new Singer 用es6的class 复制而来，不用每次都写avatar
+              id: item.Fsinger_mid,
+              name: item.Fsinger_name
+            }))
+          }
+
+          let key = item.Findex   //list的英文排行 a-z
+          if (!map[key]) {    //定义a-z的对象
+            map[key] = {
+              title: key,
+              items: []
+            }
+          }
+
+          map[key].items.push(new Singer({   //定义a-z栏目的数据
+            id: item.Fsinger_mid,
+            name: item.Fsinger_name
+          }))
+
+        })
+
+        // 把map对象转换成有序数组
+        let hot = []
+        let ret = []
+
+        for (let key in map) {
+          let val = map[key]
+          if (val.title.match(/[a-zA-Z]/)) {  //如果title有a-z，就添加到ret数组
+            ret.push(val)
+          } else if (val.title === HOT_NAME) {
+            hot.push(val)
+          }
+        }
+
+        ret.sort((a,b) => {
+          return a.title.charCodeAt(0) - b.title.charCodeAt(0) // 根据title的首字母排序
+        })
+        return hot.concat(ret)  //最后返回hot和a-z数据的组合
+
       }
+    },
+    components: {
+      // ListView
     }
   }
 
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-
+  .singer
+    position: fixed
+    top: 80px
+    width: 100%
+    bottom: 0
 </style>
